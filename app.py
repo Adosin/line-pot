1ccf70975bb5bb812e6750a18c3da1b17bf6ca89
Adosin <Adosin.Lo@gmail.com>
2019-01-20 20:00:00 +0800

    update

--- app.py
+++ app.py
@@ -36,9 +36,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text  # 使用者傳來
+    s = '你吃飯了嗎 ?'  # 機器人回傳
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text= s))
 
 
 if __name__ == "__main__":
